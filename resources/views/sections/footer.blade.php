0b94f3fd29097813e6c901f2eee9511595d6b563
root <root@DESKTOP-1U063F7>
2024-06-06 05:25:05 -0400

    updated footer

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,3 +1,21 @@
-<footer class="content-info">
-  @php(dynamic_sidebar('sidebar-footer'))
+<footer id="footer" class="relative flex flex-row items-center justify-between bg-blue-500 px-16 py-4">
+  <nav class="absolute left-0 right-0 ml-auto mr-auto flex w-2/5 justify-center">
+    <ul class="flex flex-row flex-wrap gap-4">
+      {!! wp_nav_menu([
+        'theme_location' => 'primary_navigation',
+        'container' => null,
+        'echo' => false,
+        'items_wrap' => '%3$s',
+        'walker' => new Nav_Walker
+      ]) !!}
+    </ul>
+  </nav>
+  <div class="flex w-full flex-row items-center justify-between">
+    <img src="" alt="Company Logo" />
+    <div>
+      <div>(123)-456-7890</div>
+      <div>123 STATE ST</div>
+      <div>ST ZIP-CODE</div>
+    </div>
+  </div>
 </footer>
