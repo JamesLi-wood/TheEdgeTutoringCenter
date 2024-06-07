05abafea571b83b2f75909459f5788337cf0962f
james <jamesliwood@gmail.com>
2024-06-07 03:17:57 -0400

    removed two different headers

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,28 +1,9 @@
-<!-- Desktop Header -->
-<header class="hidden h-16 flex-row items-center justify-between bg-blue-500 px-16 md:flex">
+<header id="header" class="flex min-h-16 flex-row items-center justify-between bg-blue-500 px-16">
   <img src="" alt="Company Logo" />
-  @if (has_nav_menu('primary_navigation'))
-    <nav id="desktop-nav" class="flex h-full items-center">
-      <ul class="flex h-full gap-6">
-        {!! wp_nav_menu([
-          'theme_location' => 'primary_navigation',
-          'container' => null,
-          'echo' => false,
-          'items_wrap' => '%3$s',
-          'walker' => new Nav_Walker
-        ]) !!}
-      </ul>
-    </nav>
-  @endif
-</header>
-
-<!-- Mobile Header -->
-<header class="flex h-16 flex-row items-center justify-center bg-blue-500 px-16 md:hidden">
-  <img src="" alt="Company Logo" />
-  <div id="sidebar-contents" class="absolute bottom-0 left-0 right-0 top-0 mt-10 flex flex-row transition-transform duration-500">
+  <div id="sidebar-contents" class="w-full">
     @if (has_nav_menu('primary_navigation'))
-    <nav id="mobile-nav" class="flex w-4/5 flex-col items-center bg-blue-800">
-      <ul class="mt-7 flex w-4/5 flex-col gap-4 border border-l-0 border-r-0 border-white py-5">
+      <nav id="nav">
+        <ul class="flex flex-row flex-wrap justify-end gap-3">
           {!! wp_nav_menu([
             'theme_location' => 'primary_navigation',
             'container' => null,
@@ -30,10 +11,11 @@
             'items_wrap' => '%3$s',
             'walker' => new Nav_Walker
           ]) !!}
-        </ul>
+       </ul>
       </nav>
     @endif
-    <button id="sidebar-button"></button>
+
+    <button id="sidebar-button" class="hidden"></button>
   </div>
 </header>
 
@@ -42,3 +24,4 @@
     document.getElementById("sidebar-contents").classList.toggle("hidden-content");
   });
 </script>
+
