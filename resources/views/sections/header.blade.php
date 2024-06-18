7a36631f0a426c87ec000574a951054fa3561c51
james <jamesliwood@gmail.com>
2024-06-18 01:52:02 -0400

    disable body scroll when sidebar is clicked

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,6 +1,6 @@
 <header id="header" class="flex min-h-16 flex-row items-center justify-between bg-blue-500 px-16">
   <img src="" alt="Company Logo" />
-  <div id="sidebar-contents" class="w-full">
+  <div id="sidebar-contents" class="hidden-content w-full">
     @if (has_nav_menu('primary_navigation'))
       <nav id="nav">
         <ul class="flex flex-row flex-wrap justify-end gap-3">
@@ -22,6 +22,6 @@
 <script>
   document.getElementById("sidebar-button").addEventListener("click", function() {
     document.getElementById("sidebar-contents").classList.toggle("hidden-content");
+    document.body.classList.toggle("disable-scroll");
   });
 </script>
-
