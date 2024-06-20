4de638cf3f4d9a2c6e8f8f34f1a7bd602a014267
james <jamesliwood@gmail.com>
2024-06-20 02:57:30 -0400

    added company logo

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,5 +1,13 @@
+@php
+  // Company Logo
+  $image = wp_get_attachment_image_src(343);
+@endphp
+
 <header id="header" class="flex min-h-16 flex-row items-center justify-between bg-blue-500 px-16">
-  <img src="" alt="Company Logo" />
+  <a href="{{ home_url('/homepage') }}" class="mr-5 h-32 w-4/12">
+    <img src={{ $image[0] }} alt="Company Logo" class="h-full w-full object-contain"/>
+  </a>
+  
   <div id="sidebar-contents" class="hidden-content w-full">
     @if (has_nav_menu('primary_navigation'))
       <nav id="nav">
