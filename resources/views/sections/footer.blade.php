d3aa2850ffc3c68f1af4c0fc3c63ab4bb3b8fc01
james <jamesliwood@gmail.com>
2024-06-20 03:02:42 -0400

    Added company logo. Fixed overflow when there are too many menu items

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,21 +1,30 @@
-<footer id="footer" class="relative flex flex-row items-center justify-between bg-blue-500 px-16 py-4">
-  <nav class="absolute left-0 right-0 ml-auto mr-auto flex w-2/5 justify-center">
-    <ul class="flex flex-row flex-wrap gap-4">
-      {!! wp_nav_menu([
-        'theme_location' => 'primary_navigation',
-        'container' => null,
-        'echo' => false,
-        'items_wrap' => '%3$s',
-        'walker' => new Nav_Walker
-      ]) !!}
-    </ul>
-  </nav>
-  <div class="flex w-full flex-row items-center justify-between">
-    <img src="" alt="Company Logo" />
-    <div>
-      <div>(123)-456-7890</div>
-      <div>123 STATE ST</div>
-      <div>ST ZIP-CODE</div>
-    </div>
+@php
+  // Company Logo
+  $image = wp_get_attachment_image_src(343);
+@endphp
+
+<footer id="footer" class="items-center bg-blue-500 px-16 py-4 border">
+  <a href="{{ home_url('/homepage') }}">
+    <img src={{ $image[0] }} alt="Company Logo" class="a h-32 border object-contain" />
+  </a>
+  
+  @if (has_nav_menu('primary_navigation'))
+    <nav class="b mx-3 flex justify-center">
+      <ul class="flex flex-row flex-wrap gap-4">
+        {!! wp_nav_menu([
+          'theme_location' => 'primary_navigation',
+          'container' => null,
+          'echo' => false,
+          'items_wrap' => '%3$s',
+          'walker' => new Nav_Walker
+        ]) !!}
+      </ul>
+    </nav>
+  @endif
+
+  <div class="c flex flex-col justify-self-end">
+    <div>(718) 836-3343</div>
+    <div>9008 5th Ave</div>
+    <div>NY 11209</div>
   </div>
 </footer>
