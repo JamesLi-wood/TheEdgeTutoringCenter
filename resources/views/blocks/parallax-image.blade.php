8f5454c663fb7f63ec359b4f47184b1693319033
james <jamesliwood@gmail.com>
2024-06-22 01:13:19 -0400

    added parallax image block

---
+++ resources/views/blocks/parallax-image.blade.php
@@ -0,0 +1,30 @@
+{{--
+  Title: Parallax Image
+  Description: Displays parallax image. Make sure to use a wide image.
+  Category: formatting
+  Icon: admin-comments
+  Keywords: testimonial quote
+  Mode: edit
+  PostTypes: page post
+--}}
+
+@php
+  $image = get_field('image');
+@endphp
+
+<blockquote data-{{ $block['id'] }}>
+  <div class="parallax"></div>
+</blockquote>
+
+<style>
+  .parallax {
+    background-image : url({{ $image['url'] }});
+    height: 30rem;
+    width: 100%;
+    position: relative;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+  }
+</style>
